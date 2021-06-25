9a03d0d83b251d5e1437833cfd53a136970332d2
Iryna Stein <isophiestein@gmail.com>
2021-06-24 22:29:45 -0600

    commit tree de075ed3903d2fa36f1d4dfe61af8a4f11b51537

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listings, setListings] = useState([])
+  const [searchTerm, setSearchTerm] = useState("")
+console.log(listings)
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then(resp => resp.json())
+      .then(data => setListings(data))
+  }, [])
+
+  function onDelete(id) {
+    const updatedListings = listings.filter((listing) => listing.id !== id)
+    setListings(updatedListings)
+  }
+  console.log(listings)
+
+  function searchItems(currentSearch) {
+    console.log(currentSearch)
+    setSearchTerm(currentSearch) 
+  }
+  
+  const newList = listings.filter((listing) => listing.description.toLowerCase().includes(searchTerm.toLowerCase()))
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header searchItems={searchItems} searchTerm={searchTerm} />
+      <ListingsContainer onDelete={onDelete} listings={newList} />
     </div>
   );
 }
